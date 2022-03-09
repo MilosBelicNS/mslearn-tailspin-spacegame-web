cf456c71f7499a654fa3c421eb5460a4e36b9e6d
@Shomi <milosbelic0@gmail.com>
2022-03-09 17:29:58 +0100

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Text.Json;
+using System.Threading.Tasks;
 using TailSpin.SpaceGame.Web.Models;
 
 namespace TailSpin.SpaceGame.Web
@@ -54,13 +54,13 @@ namespace TailSpin.SpaceGame.Web
             Func<T, bool> queryPredicate,
             Func<T, int> orderDescendingPredicate,
             int page = 1, int pageSize = 10
-        )
+)
         {
             var result = _items
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+                .Take(pageSize); // take items
 
             return Task<IEnumerable<T>>.FromResult(result);
         }
